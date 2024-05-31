04e73925357d614c9285f0a0b33a872904059259
Nelson Mbau <mbau.ndungu@gmail.com>
2024-05-31 09:17:57 +0300

    feat: add attributes, methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,16 @@
 class Song:
-    pass
+    artists = []
+    genres = []
+    genre_count = []
+    artist_count = []
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.artists.append(artist)
+        Song.genres.append(genre)
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
+        Song.count += 1
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
